7c37f88410242f1c4a79dcb986154d7f5f79ce24
Tiagoytsk <tiagompedra@gmail.com>
2024-03-07 09:44:40 +0000

    Ficha_2 & Ficha_3

---
+++ Ficha_2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/*int op;
+	std::cout << "Menu de opcoes\n1 - Executar o programa calculadora\n2 - Converter graus em faranheit\n3 - Converter metros em kilometros\n4 - Sair\nEscolha a opcao: ";
+	cin >> op;
+	switch (op)
+
+
+	case 1:
+		cout << "Calculadora: a executar";*/
+		int n1, n2;
+		char operador;
+		cout << "---Calculadora---\nIntroduza o primeiro numero: ";
+		cin >> n1;
+		cout << "Introduza o segundo numero: ";
+		cin >> n2;
+		cout << "Introduza o operador: ";
+		cin >> operador;
+		switch (operador) {
+		case '-':
+			cout << n1 << operador << n2 << "=" << n1 - n2;
+			break;
+		case '+':
+		default:
+			cout << "Erro operador invalido";
+			break;
+		}
+
+	/*	break;
+	case 2:
+		cout << "Conversai de temperatura: a executar";
+		break;
+	case 3:
+		cout << "Conversao de distancia: a executar";
+		break;
+	case 4:
+		cout << "A terminar o programa...";
+		break;
+	default:
+		cout << "erro opcao invalida";
+		break;
+	}*/
+}
---
+++ Ficha_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"introdza o valor: ";
+    cin>>n;
+       
+    /*while (n<=5 && n>=1)
+    {
+        cout<<n*2<<"\n";
+        n++;
+    }
+    for(n;n<=5;n++)
+    {
+        cout<<n*2<<"\n";
+    }*/
+    while(n<=15&&n>=1){
+        cout<<n<<"\n";
+        n++;
+    }
+    
+}
